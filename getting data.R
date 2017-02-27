311566074b0f12a08a5481363807b8ba95ccfc7c
sitinurulsalwa <sitinurulsalwa@yahoo.com>
2017-02-27 11:45:53 +0800

    csv read

---
+++ getting data.R
@@ -0,0 +1,7 @@
+
+
+library(readr)
+labourf <- read.csv("http://data.stats.gov.my/download_bptms.php?cat=1&id=34")
+View(bptms_Labour_force_by_marital_status_state)
+
+#if(file.exists(",\Gettingdata"))
